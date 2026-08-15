d674f5f2a27496897a53e134a3ee8342c8c1f79e
chrisholloway5 <christopher.j.holloway@outlook.com>
2026-08-15 23:47:30 +0100

    Stamp 6.2.21, build 24

--- hmailserver/source/Server/Common/Application/Version.h
+++ hmailserver/source/Server/Common/Application/Version.h
@@ -1,4 +1,4 @@
 #pragma once
                                               #define HMAILSERVER_VERSION "6.2.21"
-                                              #define HMAILSERVER_VERSION_NUMERIC 6,2,21,23
-                                              #define HMAILSERVER_BUILD 23
+                                              #define HMAILSERVER_VERSION_NUMERIC 6,2,21,24
+                                              #define HMAILSERVER_BUILD 24
