ef1335dd8e4c58b4815f8013361f0032436f7edc
chrisholloway5 <christopher.j.holloway@outlook.com>
2026-09-04 17:37:29 +0100

    6.2.23 Alpha 2, build 33

--- hmailserver/source/Server/Common/Application/Version.h
+++ hmailserver/source/Server/Common/Application/Version.h
@@ -3,5 +3,5 @@
 
 #pragma once
                                               #define HMAILSERVER_VERSION "6.2.23"
-                                              #define HMAILSERVER_VERSION_NUMERIC 6,2,23,32
-                                              #define HMAILSERVER_BUILD 32
+                                              #define HMAILSERVER_VERSION_NUMERIC 6,2,23,33
+                                              #define HMAILSERVER_BUILD 33
